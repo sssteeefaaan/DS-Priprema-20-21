a6447489decc6845396b6726a836d1c226ba4bef
Stefan Aleksic <stefan.sa.aleksic@gmail.com>
2023-09-24 23:49:17 +0200

    update

---
+++ WCF/Oktobar2023/ITournamentService.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+[ServiceContract]
+public interface ITournamentService
+{
+
+	[OperationContract]
+	bool SignIn(Student student, string disciplineId);
+
+    [OperationContract]
+    bool SignOut(string studentIndex, string disciplineId);
+
+    [OperationContract]
+    List<Discipline> GetAllDisciplinesFor(string studentIndex);
+
+    [OperationContract]
+    int GetNumberOfStudentsFor(string disciplineId);
+
+    [OperationContract]
+    List<List<Student>> GetAllGroupsFor(string disciplineId);
+}
+
+[DataContract]
+public class Discipline
+{
+    [DataMember]
+    public string Id { get; set; }
+
+    [DataMember]
+    public List<List<Student>> Groups { get; set; }
+
+    internal bool Enroll(Student student)
+    {
+        if (student == null || IsEnrolled(student.Index))
+            return false;
+
+        List<Student> group;
+        if (Groups.Count == 0 || Groups.All(g => g.Count == 16))
+        {
+            group = new List<Student>(16);
+            Groups.Add(group);
+        }
+        else
+            group = Groups.Where(g => g.Count < 16).First();
+
+        group.Add(student);
+
+        Rebalance();
+
+        return true;
+    }
+
+    internal bool IsEnrolled(string studentIndex)
+    {
+        // return Groups.Any(g => g.Any(s => s.Index == studentIndex));
+
+        foreach (List<Student> group in Groups)
+            foreach (Student student in group)
+                if (student.Index == studentIndex)
+                    return true;
+
+        return false;
+    }
+
+    internal bool Remove(string studentIndex)
+    {
+        if (!IsEnrolled(studentIndex))
+            return false;
+
+        List<Student> group = Groups.Find(g => g.Any(s => s.Index == studentIndex));
+        group.RemoveAll(s => s.Index == studentIndex);
+
+        if (group.Count == 0)
+            Groups.Remove(group);
+
+        Rebalance();
+
+        return true;
+    }
+
+    private void Rebalance()
+    {
+        List<Student> allStudents = Groups.SelectMany(g => g)
+            .OrderBy(s => -s.AverageGrade)
+            .ToList();
+
+        Groups.ForEach(g => g.Clear());
+
+        for(int i = allStudents.Count - 1; i >= 0;)
+        {
+            for (int j = Groups.Count - 1; j >= 0; j--)
+            {
+                Groups[j].Prepend(allStudents[i]);
+                allStudents.RemoveAt(i--);
+            }
+        }
+        
+    }
+}
+
+[DataContract]
+public class Student
+{
+    [DataMember]
+    public string Index { get; set; }
+
+    [DataMember]
+    public string FirstName { get; set; }
+
+    [DataMember]
+    public string LastName { get; set; }
+
+    [DataMember]
+    public decimal AverageGrade { get; set; }
+}
+
---
+++ WCF/Oktobar2023/TournamentService.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class TournamentService : ITournamentService
+{
+    private static Dictionary<string, Discipline> _disciplines = new Dictionary<string, Discipline>(10);
+
+    public List<Discipline> GetAllDisciplinesFor(string studentIndex)
+    {
+        return _disciplines.Select(keyValue => keyValue.Value)
+            .Where(discipline => discipline.IsEnrolled(studentIndex))
+            .ToList();
+    }
+
+    public List<List<Student>> GetAllGroupsFor(string disciplineId)
+    {
+        Discipline discipline;
+
+        if (!string.IsNullOrWhiteSpace(disciplineId) && _disciplines.TryGetValue(disciplineId, out discipline))
+            return discipline.Groups;
+
+        return new List<List<Student>>();
+    }
+
+    public int GetNumberOfStudentsFor(string disciplineId)
+    {
+        Discipline discipline;
+
+        if (string.IsNullOrWhiteSpace(disciplineId) || !_disciplines.TryGetValue(disciplineId, out discipline))
+            return 0;
+
+        return discipline.Groups.SelectMany(s => s).Count();
+    }
+
+    public bool SignIn(Student student, string disciplineId)
+    {
+        Discipline discipline;
+
+        if (!_disciplines.TryGetValue(disciplineId, out discipline))
+            discipline = new Discipline() { Id = disciplineId, Groups = new List<List<Student>>() };
+
+        return discipline.Enroll(student);
+    }
+
+    public bool SignOut(string studentIndex, string disciplineId)
+    {
+        Discipline discipline;
+
+        if (!_disciplines.TryGetValue(disciplineId, out discipline))
+            return false;
+
+        return discipline.Remove(studentIndex);
+    }
+}
